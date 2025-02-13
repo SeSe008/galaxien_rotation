b3188fc981bee188aa1e7cbcfe371cd8f2db28d7
SeSe008 <sebastian.radenbach@hg-gym.de>
2025-02-13 16:39:39 -0400

    Fixed Mass

--- src/utils/calculate_mass.rs
+++ src/utils/calculate_mass.rs
@@ -9,5 +9,6 @@ pub fn mass_halo(radius: f64, density_halo: f64, scale_halo: f64, iso_nfw: bool)
 }
 
 pub fn mass_disk(radius: f64, density_disk: f64, scale_disk: f64) -> f64 {
-    2.0 * PI * radius * density_disk * scale_disk * (scale_disk - (radius + scale_disk) * (-radius / scale_disk).exp())
+    let d: f64 = 1.0 / 2.0;
+    2.0 * PI * density_disk * scale_disk * d * (scale_disk - (radius + scale_disk) * (-radius / scale_disk).exp())
 }
